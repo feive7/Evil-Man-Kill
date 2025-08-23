b75cc40f5a593b78beae696b95101f870e6f2e3c
feive7 <feive7@gmail.com>
2025-08-23 15:37:43 -0700

    Enemies can crouch under obstacles now

--- RaylibFPS/glob.h
+++ RaylibFPS/glob.h
@@ -1,6 +1,7 @@
 // Global assets
 static Texture tex_john;
 static Texture tex_john_victory;
+static Texture tex_john_crouch;
 static Texture tile_1;
 static Texture tile_2;
 static Texture tile_3;
@@ -21,6 +22,7 @@ void LoadGlob() {
 	// Textures
 	tex_john = LoadTexture("assets/textures/npc_john.png");
 	tex_john_victory = LoadTexture("assets/textures/npc_john_victory.png");
+	tex_john_crouch = LoadTexture("assets/textures/npc_john_crouch.png");
 	tile_1 = LoadTexture("assets/textures/tile_1.png");
 	tile_2 = LoadTexture("assets/textures/tile_2.png");
 	tile_3 = LoadTexture("assets/textures/tile_3.png");
@@ -41,4 +43,10 @@ void LoadGlob() {
 	music_main.looping = true;
 	music_lose = LoadMusicStream("assets/audio/lost_town.mp3");
 	music_lose.looping = true;
+}
+
+void ClosestRayCollision(RayCollision& source, RayCollision compare) {
+	if (compare.hit && (!source.hit || compare.distance < source.distance)) {
+		source = compare;
+	}
 }
