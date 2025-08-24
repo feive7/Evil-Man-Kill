49bae58263aedec8e3997e61b5a4ed139cc96075
feive7 <feive7@gmail.com>
2025-08-24 09:31:51 -0700

    Re added normal lighting

--- RaylibFPS/testmap.h
+++ RaylibFPS/testmap.h
@@ -1,43 +1,13 @@
 static GameMap testmap = {
 	{ // Walls
 		{ // Cube.032
-			.points = {{4.0f,4.0f},{-4.0f,4.0f},{-4.0f,-4.0f},{4.0f,-4.0f}},
+			.points = {{3.0f,3.0f},{-3.0f,3.0f},{-3.0f,-3.0f},{3.0f,-3.0f}},
 			.z = -8.0f,
 			.height = 7.0f,
-			.tint = {0,255,248,255},
+			.tint = {255,68,62,255},
 			.surfaceMaterial = SURFACE_REGULAR,
-		},
-
-		{ // Cube.032
-			.points = {{4.0f,-4.0f},{6.0f,-2.0f},{6.0f,2.0f},{4.0f,4.0f}},
-			.z = -8.0f,
-			.height = 7.0f,
-			.tint = {0,255,248,255},
-			.surfaceMaterial = SURFACE_REGULAR,
-		},
-
-		{ // Cube.032
-			.points = {{4.0f,4.0f},{2.0f,6.0f},{-2.0f,6.0f},{-4.0f,4.0f}},
-			.z = -8.0f,
-			.height = 7.0f,
-			.tint = {0,255,248,255},
-			.surfaceMaterial = SURFACE_REGULAR,
-		},
-
-		{ // Cube.032
-			.points = {{-4.0f,4.0f},{-6.0f,2.0f},{-6.0f,-2.0f},{-4.0f,-4.0f}},
-			.z = -8.0f,
-			.height = 7.0f,
-			.tint = {0,255,248,255},
-			.surfaceMaterial = SURFACE_REGULAR,
-		},
-
-		{ // Cube.032
-			.points = {{-4.0f,-4.0f},{-2.0f,-6.0f},{2.0f,-6.0f},{4.0f,-4.0f}},
-			.z = -8.0f,
-			.height = 7.0f,
-			.tint = {0,255,248,255},
-			.surfaceMaterial = SURFACE_REGULAR,
+			.rotation = 1.0f,
+			.rotating = true,
 		},
 
 		{ // Cube.033
@@ -52,6 +22,86 @@ static GameMap testmap = {
 			.points = {{12.0f,12.0f},{-12.0f,12.0f},{-12.0f,-12.0f},{12.0f,-12.0f}},
 			.z = -9.0f,
 			.height = 1.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.035
+			.points = {{-2.65f,4.07f},{-3.01f,4.42f},{-4.42f,3.01f},{-4.07f,2.65f}},
+			.z = -8.0f,
+			.height = 7.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.036
+			.points = {{4.07f,2.65f},{4.42f,3.01f},{3.01f,4.42f},{2.65f,4.07f}},
+			.z = -8.0f,
+			.height = 7.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.037
+			.points = {{2.65f,-4.07f},{3.01f,-4.42f},{4.42f,-3.01f},{4.07f,-2.65f}},
+			.z = -8.0f,
+			.height = 7.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.038
+			.points = {{-4.07f,-2.65f},{-4.42f,-3.01f},{-3.01f,-4.42f},{-2.65f,-4.07f}},
+			.z = -8.0f,
+			.height = 7.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.039
+			.points = {{-10.0f,-6.0f},{-12.0f,-6.0f},{-12.0f,-12.0f},{-10.0f,-12.0f}},
+			.z = -8.0f,
+			.height = 14.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.040
+			.points = {{-10.0f,12.0f},{-12.0f,12.0f},{-12.0f,6.0f},{-10.0f,6.0f}},
+			.z = -8.0f,
+			.height = 14.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.041
+			.points = {{10.0f,10.0f},{10.0f,12.0f},{-10.0f,12.0f},{-10.0f,10.0f}},
+			.z = -8.0f,
+			.height = 14.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.042
+			.points = {{12.0f,12.0f},{10.0f,12.0f},{10.0f,6.0f},{12.0f,6.0f}},
+			.z = -8.0f,
+			.height = 14.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.050
+			.points = {{10.0f,-12.0f},{10.0f,-10.0f},{-10.0f,-10.0f},{-10.0f,-12.0f}},
+			.z = -8.0f,
+			.height = 14.0f,
+			.tint = {255,255,255,255},
+			.surfaceMaterial = SURFACE_REGULAR,
+		},
+
+		{ // Cube.051
+			.points = {{12.0f,-6.0f},{10.0f,-6.0f},{10.0f,-12.0f},{12.0f,-12.0f}},
+			.z = -8.0f,
+			.height = 14.0f,
 			.tint = {255,255,255,255},
 			.surfaceMaterial = SURFACE_REGULAR,
 		},
