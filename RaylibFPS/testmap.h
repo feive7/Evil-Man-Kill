f2000693242688b0dd74a4fe34a265a34558ade8
feive7 <feive7@gmail.com>
2025-08-25 08:35:58 -0700

    map to demonstrate keyframemove

--- RaylibFPS/testmap.h
+++ RaylibFPS/testmap.h
@@ -1,28 +1,28 @@
 static GameMap testmap = {
 	{ // Walls
 		{ // Cube.053
-			.points = {{1.0f,1.0f},{-1.0f,1.0f},{-1.0f,-1.0f},{1.0f,-1.0f}},
+			.points = {{6.0f,6.0f},{-6.0f,6.0f},{-6.0f,-6.0f},{6.0f,-6.0f}},
 			.z = -2.0f,
 			.height = 2.0f,
-			.tint = {0,0,255,255},
+			.tint = {0,102,0,255},
 			.surfaceMaterial = SURFACE_REGULAR,
-			.tickFunction = [](Wall* self) {
-				static const Vector2 points[] = {
-					{10,10},
-					{-10,10},
-					{-10,-10},
-					{10,-10},
-				};
-				self->keyFrameMove(points, 4);
-			}
 		},
 
-		{ // Platform
-			.points = {{50.0f,50.0f},{-50.0f,50.0f},{-50.0f,-50.0f},{50.0f,-50.0f}},
-			.z = -4.0f,
+		{ // Cube.054
+			.points = {{16.0f,5.0f},{6.0f,5.0f},{6.0f,-5.0f},{16.0f,-5.0f}},
+			.z = -2.0f,
 			.height = 2.0f,
-			.tint = {255,255,255,255},
+			.tint = {0,255,255,255},
 			.surfaceMaterial = SURFACE_REGULAR,
+			.tickFunction = [](Wall* self) {
+				static const Vector3 keyPoints[] = {
+					{11,-2,11},
+					{-11,-2,11},
+					{-11,-2,-11},
+					{11,-2,-11},
+				};
+				self->keyFrameMove(keyPoints, 4);
+			}
 		},
 
 	},
