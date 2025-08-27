f9e7dbceda7d6091a0664f9a28016ffe4d4e8433
feive7 <feive7@gmail.com>
2025-08-27 11:42:20 -0700

    Added addon for blender that adds exporting scene as header file and object properties for the game

---
+++ RaylibFPS/addon.py
@@ -0,0 +1,207 @@
+bl_info = {
+    "name": "Evil Man Kill exporter",
+    "author": "Roland Baker",
+    "version": (1, 0),
+    "blender": (3, 0, 0),
+    "description": "Addon for my game",
+    "warning": "",
+    "category": "Import-Export",
+}
+
+import bpy
+import os
+import mathutils
+
+# ---------------- Settings ----------------
+surface_materials = {"regular", "bouncy", "lava", "mud", "ladder"}
+
+# ---------------- Utilities ----------------
+def getAttributes(obj):
+    """Grab custom object attributes except RNA_UI"""
+    return {k: obj[k] for k in obj.keys() if k not in "_RNA_UI"}
+
+def FormatColor(bpyColor):
+    return [int(channel * 255) for channel in bpyColor]
+
+def FormatText(string, *replacements):
+    for r in replacements:
+        if isinstance(r, (list, tuple)):
+            for item in r:
+                string = FormatText(string, item)
+        else:
+            repl = str(round(r, 3)) if isinstance(r, (int, float)) else str(r)
+            string = string.replace("%", repl, 1)
+    return string
+
+def getPolyNormal(mesh, poly):
+    verts = [mesh.vertices[v].co for v in poly.vertices]
+    return mathutils.geometry.normal(verts)
+
+# ---------------- Wall Class ----------------
+class Wall:
+    def __init__(self):
+        self.name = ""
+        self.points = []
+        self.z = 0
+        self.height = 0
+        self.texture = ""
+        self.color = []
+        self.surfaceMaterial = "regular" 
+        self.canSpawn = False
+        self.tickFunction = ""
+        self.attributes = {}
+
+    def fromPoly(self, obj, poly):
+        self.name = obj.name
+        mesh = obj.data
+        matrix = obj.matrix_world
+
+        verts_world = [matrix @ mesh.vertices[i].co for i in poly.vertices]
+        self.points = [(-round(v.x, 2), round(v.y, 2), round(v.z, 2)) for v in verts_world]
+
+        all_z = [matrix @ v.co for v in mesh.vertices]
+        z_vals = [v.z for v in all_z]
+        self.height = round(max(z_vals) - min(z_vals), 2)
+        self.z = verts_world[0].z
+
+        self.texture = obj.game_props.texture
+        self.color = FormatColor(obj.color)
+
+        self.canSpawn = obj.game_props.can_spawn
+        self.surfaceMaterial = obj.game_props.surface_material
+        
+        
+        if obj.game_props.script_file: self.tickFunction = bpy.data.texts[obj.game_props.script_file].as_string()
+
+    def export(self):
+        cpp = FormatText("{ // %\n", self.name)
+
+        # Required
+        cpp += "\t\t\t.points = {{%f,%f},{%f,%f},{%f,%f},{%f,%f}},\n"
+        for v in self.points:
+            cpp = FormatText(cpp, v[0], v[1])
+
+        cpp += FormatText("\t\t\t.z = %f,\n", self.z)
+        cpp += FormatText("\t\t\t.height = %f,\n", self.height)
+
+        if self.texture: cpp += FormatText("\t\t\t.texture = &%,\n", self.texture.upper())
+
+        cpp += FormatText("\t\t\t.tint = {%,%,%,%},\n", self.color)
+        if self.surfaceMaterial: cpp += FormatText("\t\t\t.surfaceMaterial = SURFACE_%,\n", self.surfaceMaterial.upper())
+
+        if self.canSpawn: cpp += "\t\t\t.canSpawn = true,\n"
+
+        if self.tickFunction: 
+            cpp += "\t\t\t.tickFunction = [](Wall* self) {\n"
+            cpp += "\t\t\t\t" + self.tickFunction.replace("\n","\n\t\t\t\t") + "\n"
+            cpp += "\t\t\t},\n"
+
+        return cpp + "\t\t},\n"
+
+# ---------------- Export Logic ----------------
+def ObjectToStructs(obj):
+    mesh = obj.data
+    walls = []
+
+    for poly in mesh.polygons:
+        if round(getPolyNormal(mesh, poly).z) == -1 and len(poly.vertices) == 4:
+            wall = Wall()
+            wall.fromPoly(obj, poly)
+            walls.append(wall)
+
+    print(f"{obj.name} generated {len(walls)} walls")
+    return walls
+
+def export_to_file(filepath, map_name, walls):
+    with open(filepath, "w") as f:
+        f.write(f"static GameMap {map_name} = {{\n")
+        f.write("\t{ // Walls\n" + ''.join(f"\t\t{s.export()}\n" for s in walls) + "\t},\n")
+        f.write("};")
+
+# ---------------- Operator ----------------
+class GameProps(bpy.types.PropertyGroup):
+    surface_material: bpy.props.EnumProperty(
+        name="Surface Material",
+        items=[
+            ('regular', 'None', ''),
+            ('bouncy', 'Bouncy', ''),
+            ('lava', 'Lava', ''),
+            ('ladder', 'Ladder', ''),
+        ],
+        default='regular'
+    )
+    texture: bpy.props.StringProperty(
+        name="Texture",
+        default=""
+    )
+    script_file: bpy.props.StringProperty(
+        name="Script File"
+    )
+    can_spawn: bpy.props.BoolProperty(
+        name="Can Spawn"
+    )
+
+# Define a panel so it shows up in the UI
+class OBJECT_PT_GamePropsPanel(bpy.types.Panel):
+    bl_label = "Game Properties"
+    bl_idname = "OBJECT_PT_game_props_panel"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"  # Shows up in the Object Properties tab
+
+    def draw(self, context):
+        layout = self.layout
+        props = context.object.game_props   # get the PropertyGroup
+
+        layout.prop(props, "surface_material")
+        layout.prop(props, "texture")
+        layout.prop(props, "script_file")
+        layout.prop(props, "can_spawn")
+        
+class EXPORT_OT_walls_to_cpp(bpy.types.Operator):
+    """Export Walls to C++ Map"""
+    bl_idname = "export_scene.walls_to_cpp"
+    bl_label = "Export Walls to C++ Map"
+    bl_options = {"REGISTER", "UNDO"}
+
+    filepath: bpy.props.StringProperty(
+        name="File Path",
+        description="Where to export the C++ header",
+        subtype="FILE_PATH",
+        default="C:/Users/rolan/OneDrive/Documents/Roland C++/RaylibFPS/RaylibFPS/testmap.h"
+    )
+
+    map_name: bpy.props.StringProperty(
+        name="Map Name",
+        description="Name of the exported GameMap",
+        default="testmap"
+    )
+
+    def execute(self, context):
+        walls = []
+        for obj in bpy.data.objects:
+            if obj and obj.visible_get() and obj.type == 'MESH':
+                walls.extend(ObjectToStructs(obj))
+
+        export_to_file(self.filepath, self.map_name, walls)
+        self.report({'INFO'}, f"Exported {len(walls)} walls to {self.filepath}")
+        return {"FINISHED"}
+
+# ---------------- Registration ----------------
+def menu_func_export(self, context):
+    self.layout.operator(EXPORT_OT_walls_to_cpp.bl_idname, text="Walls to C++ Map (.h)")
+
+def register():
+    bpy.utils.register_class(EXPORT_OT_walls_to_cpp)
+    bpy.utils.register_class(OBJECT_PT_GamePropsPanel)
+    bpy.utils.register_class(GameProps)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+    bpy.types.Object.game_props = bpy.props.PointerProperty(type=GameProps)
+
+def unregister():
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+    bpy.utils.unregister_class(EXPORT_OT_walls_to_cpp)
+    del bpy.types.Object.game_props  # clean up property on unregister
+
+if __name__ == "__main__":
+    register()
